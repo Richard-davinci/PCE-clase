54d278a8636b3606bc8bb9e5c9b30a873ef0d0c2
ricardo garcia <ricardogarci4@gmail.com>
2025-09-09 13:27:20 -0300

    Introducción a Controllers.

--- clase01/routes/web.php
+++ clase01/routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
+
+Route::get('/', [\App\Http\Controllers\HomeController::class, 'home']);
+
+Route::get('/quienes-somos', [\App\Http\Controllers\AboutController::class, 'about']);
