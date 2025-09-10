47b8e1e3c27d3163b606050d0f71b1563e890502
ricardo garcia <ricardogarci4@gmail.com>
2025-09-09 23:27:11 -0300

    add movies y URLs absolutas

--- clase01/routes/web.php
+++ clase01/routes/web.php
@@ -9,3 +9,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [\App\Http\Controllers\HomeController::class, 'home']);
 
 Route::get('/quienes-somos', [\App\Http\Controllers\AboutController::class, 'about']);
+
+Route::get('peliculas/listado', [\App\Http\Controllers\MoviesController::class, 'index']);
